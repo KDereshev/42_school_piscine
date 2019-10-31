9c201b73fad8fb1bc9c1fb2bca9778cdd1227fe2
Konstantin Dereshev <kdereshe@e1z1r6p6.42.us.org>
2019-10-30 19:56:02 -0700

    fix day 06 ex01" " wqew

--- main/day06/ex01/ft_print_program_name.c
+++ main/day06/ex01/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: kdereshe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 16:30:49 by kdereshe          #+#    #+#             */
-/*   Updated: 2019/10/30 18:45:54 by kdereshe         ###   ########.fr       */
+/*   Updated: 2019/10/30 19:43:56 by kdereshe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@ void	ft_putchar(char c);
 
 int		main(int argc, char **argv)
 {
-	if (argc > 0)
-		while (*argv[0])
-			ft_putchar(*argv[0]++);
+	(void)argc;
+	while (*argv[0])
+		ft_putchar(*argv[0]++);
 	ft_putchar('\n');
 	return (0);
 }
