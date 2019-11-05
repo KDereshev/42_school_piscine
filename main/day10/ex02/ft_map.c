f575f4a6ade28da198b10821dcac0a00395fe36f
Konstantin Dereshev <kdereshe@e1z1r6p6.42.us.org>
2019-11-05 15:08:20 -0800

    add day10

---
+++ main/day10/ex02/ft_map.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdereshe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/04 16:19:25 by kdereshe          #+#    #+#             */
+/*   Updated: 2019/11/05 13:05:07 by kdereshe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_map(int *tab, int length, int (*f)(int))
+{
+	int	i;
+	int	*result;
+
+	i = 0;
+	result = (int*)malloc(sizeof(int) * length);
+	while (i < length)
+	{
+		result[i] = (*f)(tab[i]);
+		++i;
+	}
+	return (result);
+}
